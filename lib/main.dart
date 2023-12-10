9d6ec075fe61a14df20e2cacb32df7fb396425b8
RON VANDA <ronvanda99@gmail.com>
2023-12-10 11:22:41 +0700

    new_expense_theming

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:pnt_expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 211, 206, 206),
+      ),
       home: const Expenses(),
     ),
   );
